335cdcf1accd76781c518702eaa32026dffefbcd
Patricia Dörr <pdoerr@freenet.de>
2026-05-04 20:19:38 +0200

    Hoffentlich läuft nun js auf vercel

--- src/umwelttrier/app/main.py
+++ src/umwelttrier/app/main.py
@@ -9,10 +9,7 @@ from .utils.load_data import listen_for_notifications
 from .layouts.callbacks import register_callbacks
 
 server = Flask(__name__)
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-external_scripts = ['https://cdn.plot.ly/plotly-latest.min.js']
-app = Dash(__name__, server=server, external_stylesheets=external_stylesheets,
-           external_scripts=external_scripts, suppress_callback_exceptions=True)
+app = Dash(__name__, server=server, suppress_callback_exceptions=True, assets_folder='assets')
 
 
 app.layout = html.Div([
