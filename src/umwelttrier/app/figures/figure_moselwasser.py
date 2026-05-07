276d9e873636185b4ce4bf564f31d92063e51a45
= <=>
2026-05-07 09:01:35 +0200

    Linter Warnung behoben

--- src/umwelttrier/app/figures/figure_moselwasser.py
+++ src/umwelttrier/app/figures/figure_moselwasser.py
@@ -5,6 +5,8 @@ def update_moseltemperatur_und_fisch():
     assets_dir = "/assets"
     query = text("SELECT wert, zeitpunkt FROM wassertemperatur_mosel WHERE zeitpunkt = (SELECT MAX(zeitpunkt) FROM wassertemperatur_mosel)")
     result = load_data.make_query(query)
+    if result is None:
+        return "Keine Daten verfügbar", assets_dir+'/images/sad_fish.jpeg'
     (mosel_temperatur, datum) = result.fetchone()
     mosel_temperatur = round(mosel_temperatur,1)
     datumString = datum.strftime("%d.%m.%y")
